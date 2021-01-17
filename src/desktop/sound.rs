f3d2d8124cb0c9b0b74f1fca4b43da4ecaa27051
MinusKelvin <mark.carlson@minuskelvin.net>
2021-01-17 12:43:54 +1100

    reimplement desktop sound backend

--- src/desktop/sound.rs
+++ src/desktop/sound.rs
@@ -1,22 +1,72 @@
 use std::fs::File;
 use std::io::BufReader;
-use std::sync::mpsc::channel;
+use std::sync::mpsc::{channel, Receiver};
 
 use futures::channel::mpsc::UnboundedReceiver;
 use futures::StreamExt;
-use rodio::source::Buffered;
+use rodio::source::{Buffered, UniformSourceIterator};
 use rodio::{Decoder, OutputStream, Sink, Source};
 
 use crate::sound::SoundCommand;
 
-pub(crate) type InternalSound = Buffered<Decoder<BufReader<File>>>;
+pub(crate) type InternalSound = Buffered<UniformSourceIterator<Decoder<BufReader<File>>, i16>>;
+
+const CHANNELS: u16 = 2;
+const SAMPLE_RATE: u32 = 44_100;
 
 pub(crate) async fn load(source: &str) -> Result<InternalSound, String> {
-    Ok(Decoder::new(BufReader::new(
-        File::open(source).map_err(|e| e.to_string())?,
-    ))
-    .map_err(|e| e.to_string())?
+    Ok(UniformSourceIterator::new(
+        Decoder::new(BufReader::new(
+            File::open(source).map_err(|e| e.to_string())?,
+        ))
+        .map_err(|e| e.to_string())?,
+        CHANNELS,
+        SAMPLE_RATE,
+    )
     .buffered())
+}
+
+struct Mixer {
+    active: Vec<InternalSound>,
+    incoming: Receiver<InternalSound>,
+}
+
+impl Iterator for Mixer {
+    type Item = i16;
+
+    fn next(&mut self) -> Option<i16> {
+        self.active.extend(self.incoming.try_iter());
+
+        let mut sample_value = 0i16;
+        for i in (0..self.active.len()).rev() {
+            match self.active[i].next() {
+                Some(v) => sample_value = sample_value.saturating_add(v),
+                None => {
+                    self.active.swap_remove(i);
+                }
+            }
+        }
+
+        Some(sample_value)
+    }
+}
+
+impl Source for Mixer {
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    fn channels(&self) -> u16 {
+        CHANNELS
+    }
+
+    fn sample_rate(&self) -> u32 {
+        SAMPLE_RATE
+    }
+
+    fn total_duration(&self) -> Option<std::time::Duration> {
+        None
+    }
 }
 
 pub(crate) async fn sound_service(mut source: UnboundedReceiver<SoundCommand>) {
@@ -24,20 +74,31 @@ pub(crate) async fn sound_service(mut source: UnboundedReceiver<SoundCommand>) {
 
     std::thread::spawn(move || {
         let (_stream, handle) = OutputStream::try_default().unwrap();
-        let mut sinks: Vec<Sink> = vec![];
+        let (mut sound_send, incoming) = channel();
+        let sink = Sink::try_new(&handle).unwrap();
+        sink.append(Mixer {
+            active: vec![],
+            incoming,
+        });
+
         while let Ok(cmd) = recv.recv() {
-            sinks.retain(|s| !s.empty());
             match cmd {
                 SoundCommand::Play(sound) => {
-                    if let Ok(sink) = Sink::try_new(&handle) {
-                        sink.append(sound);
-                        sinks.push(sink);
-                    }
+                    sound_send.send(sound).ok();
                 }
-                SoundCommand::Pause => sinks.iter().for_each(Sink::pause),
-                SoundCommand::Resume => sinks.iter().for_each(Sink::play),
-                SoundCommand::Stop => sinks.clear(),
-                SoundCommand::SetVolume(volume) => sinks.iter().for_each(|s| s.set_volume(volume)),
+                SoundCommand::Pause => sink.pause(),
+                SoundCommand::Resume => sink.play(),
+                SoundCommand::Stop => {
+                    sink.stop();
+                    sink.play();
+                    let (s, incoming) = channel();
+                    sound_send = s;
+                    sink.append(Mixer {
+                        active: vec![],
+                        incoming
+                    });
+                },
+                SoundCommand::SetVolume(volume) => sink.set_volume(volume),
             }
         }
     });
